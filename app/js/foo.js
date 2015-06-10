a047e74470b8c5493934ed1f59f58dba60e4ab30
Joe Joyave <jjoyave1@gmail.com>
2015-06-09 21:55:41 -0400

    initial commit, foo.js working

---
+++ app/js/foo.js
@@ -0,0 +1,24 @@
+window.Foo = (function() {
+
+  function Foo() {
+
+    this.plusOne = function (x) {
+      return x + 1;
+    }
+
+    this.sortArray = function(array) {
+      return array.concat().sort();
+    }
+
+    this.hasValue = function(array, item) {
+      return array.indexOf(item) != -1;
+    }
+  }
+
+  Foo.greet = function(string) {
+    return "Hello, " + string + "!";
+  }
+
+  return Foo;
+
+})();
